34679751d804202aa66078532e8eba2b70c89e1b
Vladan Milojevic <40184129+vmilojevic@users.noreply.github.com>
2025-10-14 16:18:45 +0200

    feat: add more tail sampling policy examples

---
+++ opentelemetry-collector/tail-sampling/06_send-spans-ottl-condition-sampled.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+source ../../.env
+
+# Single trace ID to be shared across all spans
+TRACE_ID="$(date +%s)805a7c87bc2f6dab94a7f1"
+
+curl http://localhost:4318/v1/traces \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '
+{
+  "resourceSpans": [
+    {
+      "resource": {
+        "attributes": [
+          {
+            "key": "service.name",
+            "value": { "stringValue": "frontend" }
+          },
+          {
+            "key": "policy.group",
+            "value": { "stringValue": "ottl-condition-sampling" }
+          }
+        ]
+      },
+      "scopeSpans": [
+        {
+          "schemaUrl": "",
+          "scope": {
+            "attributes": []
+          },
+          "spans": [
+            {
+              "attributes": [
+                {
+                  "key": "endpoint.name",
+                  "value": { "stringValue": "OTLP via HTTP" }
+                }
+              ],
+              "startTimeUnixNano": "'$(date +%s%N)'",
+              "endTimeUnixNano": "'$(date +%s%N)'",
+              "events": [],
+              "flags": 0,
+              "kind": 1,
+              "links": [],
+              "name": "Manually Ingested Span",
+              "traceId": "'$TRACE_ID'",
+              "parentSpanId": "",
+              "spanId": "e12ea8f8e32c0e61",
+              "traceState": "",
+              "status": {
+                "code": 0,
+                "message": "Frontend - UNSET"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}'
