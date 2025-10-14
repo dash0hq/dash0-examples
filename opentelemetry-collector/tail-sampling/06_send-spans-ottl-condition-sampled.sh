1ccead55a25e104066d5339e29b6aeca4b02ed1a
Vladan Milojevic <40184129+vmilojevic@users.noreply.github.com>
2025-10-14 16:35:29 +0200

    feat: add span event ottl condition example

--- opentelemetry-collector/tail-sampling/06_send-spans-ottl-condition-sampled.sh
+++ opentelemetry-collector/tail-sampling/06_send-spans-ottl-condition-sampled.sh
@@ -4,9 +4,6 @@ set -eo pipefail
 
 source ../../.env
 
-# Single trace ID to be shared across all spans
-TRACE_ID="$(date +%s)805a7c87bc2f6dab94a7f1"
-
 curl http://localhost:4318/v1/traces \
   -X POST \
   -H "Content-Type: application/json" \
@@ -47,9 +44,75 @@ curl http://localhost:4318/v1/traces \
               "kind": 1,
               "links": [],
               "name": "Manually Ingested Span",
-              "traceId": "'$TRACE_ID'",
+              "traceId": "'$(date +%s)805a7c87bc2f6dab94a7f1'",
               "parentSpanId": "",
               "spanId": "e12ea8f8e32c0e61",
+              "traceState": "",
+              "status": {
+                "code": 0,
+                "message": "Frontend - UNSET"
+              }
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}'
+
+# Simulate minimal latency
+sleep .1
+
+curl http://localhost:4318/v1/traces \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d '
+{
+  "resourceSpans": [
+    {
+      "resource": {
+        "attributes": [
+          {
+            "key": "service.name",
+            "value": { "stringValue": "frontend" }
+          }
+        ]
+      },
+      "scopeSpans": [
+        {
+          "schemaUrl": "",
+          "scope": {
+            "attributes": []
+          },
+          "spans": [
+            {
+              "attributes": [
+                {
+                  "key": "endpoint.name",
+                  "value": { "stringValue": "OTLP via HTTP" }
+                }
+              ],
+              "startTimeUnixNano": "'$(date +%s%N)'",
+              "endTimeUnixNano": "'$(date +%s%N)'",
+              "events": [
+                {
+                  "timeUnixNano": "'$(date +%s%N)'",
+                  "name": "example.event",
+                  "attributes": [
+                    {
+                      "key": "example.attribute",
+                      "value": { "stringValue": "example.value" }
+                    }
+                  ]
+                }
+              ],
+              "flags": 0,
+              "kind": 1,
+              "links": [],
+              "name": "Manually Ingested Span",
+              "traceId": "'$(date +%s)805a7c87bc2f6dab94a7f2'",
+              "parentSpanId": "",
+              "spanId": "e12ea8f8e32c0e62",
               "traceState": "",
               "status": {
                 "code": 0,
